471cd2690c2c21c2f033a86397e54dc30d62587d
vinayak149 <vinayak.sharma7149@gmail.com>
2023-09-14 23:33:20 +0530

    app

---
+++ routes/todo.js
@@ -0,0 +1,30 @@
+const router = require('express').Router()
+const Todo = require("../models/Todo")
+
+
+
+//routes
+router.post("/add/todo", (req, res) => {
+    const { todo } = req.body;
+    const newTodo = new Todo({ todo })
+
+    newTodo.save().then(() => {
+        res.redirect("/")
+    })
+        .catch((err) => console.log(err));
+
+})
+
+
+
+    .get("delete/todo/:_id", (req, res) => {
+        const { _id } = req.params;
+        Todo.deleteOne({ _id })
+            .then(() => {
+                console.log("Deleted task succesfully")
+                res.redirect("/")
+            })
+            .catch((err) => console.log(err));
+    })
+
+module.exports = router;
